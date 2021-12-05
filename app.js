d960abe87f14fc97966a93912c6fc2303f9458e7
Jerry <jerrywei998@gmail.com>
2021-12-05 14:33:41 -0500

    Finished Capture user input

---
+++ app.js
@@ -0,0 +1,47 @@
+// command line argv
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+    name: 'Lernantino',
+    age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+      console.log(profileItem)
+    });
+
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
+printProfileData(profileDataArgs);
+
+
+// // Using function expression syntax
+// const addNums = function (numOne, numTwo) {
+//     return numOne + numTwo;
+// };
+
+// // Using new arrow function syntax
+// const addNums = (numOne, numTwo) => {
+//     return numOne + numTwo;
+// };
+
+// const addNums = (numOne, numTwo) => numOne + numTwo;
